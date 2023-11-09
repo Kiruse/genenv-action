a7624d6e9d96c65675a8eb26312ab3bed5bf654f
kiruse <26940742+Kiruse@users.noreply.github.com>
2023-11-09 18:08:37 +0100

    add require input

--- dist/index.js
+++ dist/index.js
@@ -25942,6 +25942,15 @@ try {
   const escape = (v) => v.replace(/\\/g, '\\\\').replace(/'/g, "\\'");
   const lines = core.getMultilineInput('env', { required: true });
   const vars = lines.map(line => line.trim().split('=').map(s => s.trim()));
+
+  const required = core.getInput('require').split(/[,\n]+/).map(s => s.trim());
+  for (const requiredVar of required) {
+    if (!vars.find(([k]) => k === requiredVar)) {
+      core.setFailed(`Required variable ${requiredVar} is missing`);
+      process.exit(1);
+    }
+  }
+
   fs.writeFileSync('.env', vars.map(([k, v]) => `${k}='${escape(v)}'`).join('\n'));
 } catch (err) {
   core.setFailed(err.message);
